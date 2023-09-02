d08f754e0a019af37d83eb65cb909139c15b2d6a
taufikikrom <143859368+taufikikrom@users.noreply.github.com>
2023-09-02 10:43:14 +0700

    commit pertama

---
+++ p1.py
@@ -0,0 +1,35 @@
+# apel=52.2
+# print(apel)
+# prin9(type(apel))
+# apel12=52
+# print(type(apel12))
+# nama_apel="malang"
+# print(nama_apel)
+# print(type(nama_apel))
+# apel_benar = true
+# apel_salah = False
+# print(type(apel_benar))
+# print(apel_salah)
+
+# list_warna_apel = ['hijau','merah']
+# list_apel1 =['merah',2,3.0]
+# print(list_warna_apel)
+# print(list_apel1)
+
+# tuple_jumlah = (2,5.25,satu)
+# print(tuple_jumlah) 
+
+# apel_dict ={'nama':'apel','alamat':'malang','jumlah':}
+# print(apel_dict['nama'])
+# print(apel_dict['jumlah'])
+
+print (34+2)
+print(39)
+print(2*6)
+b=39
+e=4 
+print(b//e)
+
+b=3
+e=4
+print(b**e)
